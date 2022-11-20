155089550628596ff1cddd9b987566b3e30beda8
YeoJiSu <duwltn1301@naver.com>
2022-11-20 15:49:28 +0900

    BAM 알고리즘 예제 구현

--- BAM.py
+++ BAM.py
@@ -1,40 +1,78 @@
 import numpy as np
+import copy
 
 A1 = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
 A2 = [1,1,0,0,1,1,0,0,1,1,0,0,1,1,0]
 A3 = [1,1,1,0,0,0,1,1,1,0,0,0,1,1,1]
 A4 = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0]
 
-def activation(list):
-    for i in range(len(list)):
-        if list[i] > 0:
-            list[i] = 1
+B1 = [1,1,1,1,0,0,0,0,1,1]
+B2 = [1,1,1,0,0,0,1,1,1,0]
+B3 = [1,1,0,0,1,1,0,0,1,1]
+B4 = [1,0,1,0,1,0,1,0,1,0]
+
+def activation(_list):
+    acList = copy.deepcopy(_list)
+    for i in range(len(acList)):
+        if acList[i] > 0:
+            acList[i] = 1
         else:
-            list[i] = 0
-    return list
+            acList[i] = 0
+    return acList
 
-def bipolar(list):
-    for i in range(len(list)):
-        if list[i] > 0:
-            list[i] = 1
+def bipolar(_list):
+    biList = copy.deepcopy(_list)
+    for i in range(len(biList)):
+        if biList[i] > 0:
+            biList[i] = 1
         else:
-            list[i] = -1
-    return list
+            biList[i] = -1
+    return biList
 
-# def weight(list):
-#     w = []
-    
-#     for i in range(1,len(list)):
-#         x = np.array(list[i])
-#         for j in range(len(x)):
-#             x[j]*x
-#     return w
+_A1 = np.array(bipolar(A1))
+_A2 = np.array(bipolar(A2))
+_A3 = np.array(bipolar(A3))
+_A4 = np.array(bipolar(A4))
 
-A1 = bipolar(A1)
-A2 = bipolar(A2)
-A3 = bipolar(A3)
-A4 = bipolar(A4)
+_B1 = np.array(bipolar(B1))
+_B2 = np.array(bipolar(B2))
+_B3 = np.array(bipolar(B3))
+_B4 = np.array(bipolar(B4))
 
-w = [A1,A2,A3,A4]
-# print(weight(w))
-     
+
+w1 = []
+w2 = []
+w3 = []
+w4 = []
+for j in range(len(_A1)):
+    w1.append(_A1[j]*_B1)
+    w2.append(_A2[j]*_B2)
+    w3.append(_A3[j]*_B3)
+    w4.append(_A4[j]*_B4)
+
+w1 = np.array(w1)
+w2 = np.array(w2)
+w3 = np.array(w3)
+w4 = np.array(w4)
+
+weight = w1+w2+w3+w4
+print("가중치:\n", weight)
+newA1 = np.array(A1[0]*weight[0])
+for i in range(1,len(weight)):
+    newA1 += np.array(A1[i]*weight[i])
+print("\nA1*W:\n",newA1)
+acNewA1= np.array(activation(newA1))
+print("activation 적용후 \n",acNewA1)
+
+transWeight = np.transpose(weight)
+newB1 = np.array(B1[0]*transWeight[0])
+for i in range(1,len(transWeight)):
+    newB1 += np.array(B1[i]*transWeight[i])
+print("\nB1*W:\n",newB1)
+acNewB1 = np.array(activation(newB1))
+print("activation 적용후 \n",acNewB1)
+print("A1 값과 동일하다\n",A1)
+
+
+################################################################################
+# deep copy를 이용하여 리스트 복사 이후 활용할 떄 복사 당한 리스트가 변경되는 것 막음.
